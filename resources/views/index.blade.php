51042b5b1902f20e4bbf0b9d1a76c1a61c44b7ef
Unknown11-spec <farelghzl06@gmail.com>
2023-02-03 08:08:07 +0700

    Hasil awal

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+    <!-- <link rel="stylesheet" href="./css/app.css"> -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="app">
+        <header>
+            <h1>Lets chatting</h1>
+            <input type="text" name="username" id="username" placeholder="Enter a username">
+        </header>
+    </div>
+
+    <div id="message"></div>
+
+    <div>
+        <form id="message_form">
+            <input type="text" name="message" id="message_input" placeholder="Write a message">
+            <button type="submit" id="message_send"> Send message </button>
+        </form>
+    </div>
+    
+
+    <script src="/js/app.js"></script>
+</body>
+</html>
